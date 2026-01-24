96b68c5c65163f297751b4940a0312cf83ce1cd1
Mdabdullah3 <mdabdullahnoman777@gmail.com>
2026-01-24 13:47:47 +0600

    up

--- src/app/api/score/route.tsx
+++ src/app/api/score/route.tsx
@@ -1,17 +1,23 @@
+// app/api/score/route.ts
 import { NextResponse } from 'next/server';
+
 export async function GET(request: Request) {
     const { searchParams } = new URL(request.url);
     const wallet = searchParams.get('wallet');
+
     if (!wallet) return NextResponse.json({ error: 'Wallet required' }, { status: 400 });
+
     try {
         const res = await fetch(`https://api.fairscale.xyz/score?wallet=${wallet}`, {
             headers: {
-                'fairkey': '1582cfee47d73f1d72455af8fbf0c9752b22f3e69a720eda6d95616e7a007ddc' 
+                'fairkey': '1582cfee47d73f1d72455af8fbf0c9752b22f3e69a720eda6d95616e7a007ddc'
             }
         });
 
         if (!res.ok) throw new Error('API Error');
         const data = await res.json();
+        console.log(data, "data load");
+
         return NextResponse.json(data);
     } catch (error) {
         return NextResponse.json({ error: 'Failed to fetch score' }, { status: 500 });
